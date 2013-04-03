3b0cc84945ef462cb9356e5d990273b8dd2917f7
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2013-04-03 19:24:46 +0200

    Added new mark warning, improved styling

--- E-Dnevnik enchancer.js
+++ E-Dnevnik enchancer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       E-Dnevnik enhancer
 // @namespace  -
-// @version    0.4
+// @version    0.5
 // @description Set of enhancements for E-Dnevnik project
 // @match      https://ocjene.skole.hr/pregled/ocjene/*
 // @copyright  2013+, Nikola Bunjevac
@@ -9,106 +9,121 @@
 // @require    http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js
 // ==/UserScript==
 
-$(document).ready(function() {
+$(document).ready(function () {
     var averages = [];
     var badMarks = 0;
-	var toEscape = [];
+    var toEscape = [];
+    var lSA = (localStorage != "undefined"); //localStorageAvailable
+    if (lSA && localStorage.getItem("markc") === null) {
+        localStorage.markc = JSON.stringify([]);
+    }
     
-	// subject mark averages
-    $(".grades > table:first-child").each(function(index) {
+
+    // subject mark averages
+	var markc = [];
+    $(".grades > table:first-child").each(function (index) {
         var sum = 0;
         var count = 0;
         var content = $(this).html();
         content = content.replace(/, /g, ""); // for handling more marks in one <td>
         var matcher = new RegExp(">[1-5]+<");
         var resp = 1;
-        while (resp != null) {
+        while (resp !== null) {
             resp = matcher.exec(content);
-            if (resp != null) {
+            if (resp !== null) {
                 var ind = resp.index + 1;
                 while (content[ind] !== '<') { // ...more marks in one <td>
-                    sum += parseInt(content[ind], 10);  
+                    sum += parseInt(content[ind], 10);
                     count++;
                     ind++;
                 }
                 content = content.slice(ind + 1, content.length);
             }
         }
-        if (count === 0 || parseFloat(sum/count).toFixed(2) === 0) {
-			toEscape.push(index);
-			return null; // no marks, leave it empty
-		}
+        if (count === 0 || parseFloat(sum / count).toFixed(2) === 0) {
+            toEscape.push(index);
+            return null; // no marks, leave it empty
+        }
+		markc.push(count);
         averages.push(parseFloat(sum / count).toFixed(2));
-        $("tr:contains('ZAKLJUÈENO')", this).after('<tr><td class="activity bold">PROSJEK</td> <td colspan="10" class="t-center bold">' + averages[index] +  '</td></tr>');
+        $("tr:contains('ZAKLJUÈENO')", this).after('<tr><td class="activity bold">PROSJEK</td> <td colspan="10" class="t-center bold">' + averages[index] + '</td></tr>');
     });
-    
+
     if (averages.length === 0) return null; // no marks
-    
+
     var sum = 0;
     for (var i = 0; i < averages.length; i++) {
         var m = averages[i];
-        if (m - parseInt(m, 10) < 0.5)
-        	m = Math.floor(m);
-        else
-            m = Math.ceil(m);
+        if (m - parseInt(m, 10) < 0.5) m = Math.floor(m);
+        else m = Math.ceil(m);
         sum += m;
     }
-    
-	// adds averages beside profesor's names
+
+    // adds averages beside profesor's names
     var mark = sum / averages.length;
     var offset = 0;
-    $(".course").each(function(index) {
-		for (var j = 0; j < toEscape.length; j++)
-			if (index == toEscape[j]) {
-				offset++;
-				return null;
-			}
+	var markcc = [];
+	if (lSA) {
+		markcc = JSON.parse(localStorage.markc);
+	}
+    $(".course").each(function (index) {
+        for (var j = 0; j < toEscape.length; j++)
+            if (index == toEscape[j]) {
+                offset++;
+                return null;
+            }
         $(".course-info", this).append(', <span id="prosjek" style="color:#baf">Prosjek: ' + averages[index - offset] + '</span>');
         if (averages[index - offset] < 2.0) {
             badMarks++;
             $("#prosjek", this).append(', <span style="color:red; font-weight: bold;">Trenutno padaš ovaj predmet!</span>');
         }
+        if (lSA && markcc.length === markc.length && markcc[index - offset] != markc[index - offset]) {
+            $("#prosjek", this).append(', <span style="color: #afa">Nova ocjena!</span>'); //if there's a new mark, show it
+        }
     });
-    
-	// final average mark and bad marks warning
+
+    if (lSA) {
+        localStorage.markc = JSON.stringify(markc); //save new averages for future checking
+    }
+
+    // final average mark and bad marks warning
     $(".sectionTitle").after('<div id="ukpros" style="color:#f55"><strong>Trenutni ukupni prosjek:</strong> ' + parseFloat(mark).toFixed(2) + '</div><br />');
-    if (badMarks > 0)
-        $("#ukpros").after('<div style="color:#f55"><strong>Trenutno imaš jedinica: ' + badMarks + '</strong></div>');
-    
-    $("#student-class").after('<div id="testovi" style="position: absolute; float: both; top: 75px; left: 10px; padding: 5px; width: 200px;"><br />' + 
-								'<table id="tt"><th>Pisane provjere</th><tbody><tr id="prvi"></tr></tbody></table></div>');
-    
-	// exam table
+    if (badMarks > 0) $("#ukpros").after('<div style="color:#f55"><strong>Trenutno imaš jedinica: ' + badMarks + '</strong></div>');
+
+    $("#student-class").after('<div id="testovi" style="position: absolute; float: both; top: 10px; left: 10px; padding: 5px; width: 200px;"><br />' +
+        '<table id="tt"><th>Pisane provjere <br><span style="font-size: 10px"><i>(povuci za premještanje)</i></span></th><tbody><tr id="prvi"></tr></tbody></table></div>');
+
+    // exam table
     var curr_date = new Date();
     var dates = [];
-    
-    $(".grades").each(function() {
-        $(".tasks-title:contains('Raspored')", this).next().children("tbody").children("tr").each(function() {
+
+    $(".grades").each(function () {
+        $(".tasks-title:contains('Raspored')", this).next().children("tbody").children("tr").each(function () {
             if (!($(this).hasClass("inactive")) && $(this).children().length > 0 && $(this).children().is("td")) {
                 var date = $(this).children("td").eq(0).html();
                 var test = $(this).children("td").eq(1).html();
                 var subject = $(this).parents().eq(3).prev().attr("id");
                 var date_obj = new Date(date.slice(6, 10), date.slice(3, 5) - 1, date.slice(0, 2));
                 var css = "";
-                if (Math.round((date_obj-curr_date)/1000/60/60/24) <= 7)
-                    css = "background-color: #fbb;";
-                dates.push([date_obj, '<tr style="' + css + '"><td class="datum" style="background-color: #dde">' + date + 
-                            '</td><td style="background-color: #ddd">' + test + "</td><td>" + subject + "</td></tr>"]);   
+                if (Math.round((date_obj - curr_date) / 1000 / 60 / 60 / 24) <= 7) css = "background-color: #fbb;";
+                else css = "background-color: #ddf;";
+                dates.push([date_obj, '<tr style="' + css + '"><td class="datum" style="background-color: #ddf">' + date +
+                    '</td><td style="background-color: #ddd">' + test + '</td><td>' + subject + "</td></tr>"]);
             }
         });
     });
-    
-	// date sorting
-    var sort_asc = function(d1, d2) {
+
+    // date sorting
+    var sort_asc = function (d1, d2) {
         if (d1[0] > d2[0]) return 1;
         return -1;
     };
-    
+
     dates.sort(sort_asc);
-    
-    for (var i = 0; i < dates.length; i++) {
+
+    for (i = 0; i < dates.length; i++) {
         $("#prvi").append(dates[i][1]);
     }
-    
+
     $("#testovi").draggable();
 });
